ee47499eb64aec09571730fc941ad0b82fb395d9
Acidal <113132307+Acidal@users.noreply.github.com>
2023-03-02 20:24:55 -0800

    Push
    aaa

---
+++ Assets/Scripts/InteractiveMechanics.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveMechanics : MonoBehaviour
+{
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Interactive")
+        {
+            Debug.Log("hit a collider!");
+        }
+        
+    }
+
+}
